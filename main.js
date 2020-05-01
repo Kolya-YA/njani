cb078ced242b0c386716216680eae9ed0d8a2648
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-05-01 18:29:41 +0200

    strings

---
+++ main.js
@@ -0,0 +1,34 @@
+// var drawCats = function (howManyTimes) {
+//   for (var i = 1; i <= howManyTimes; i++) {
+//     console.log(`Meow #${i} =^.^=`);
+//   }
+// };
+// drawCats(8);
+
+// var secondsInAMinute = 60;
+// var minutesInAHour = 60;
+// var secondsInAHour = secondsInAMinute * minutesInAHour;
+// var hoursInADay = 24;
+// var secondsInADay = secondsInAHour * hoursInADay;
+// var daysInAYear = 365;
+// var secondsInAYear = secondsInADay * daysInAYear;
+// var veronikasAge = 10;
+// var veronikaspapaAge = 48;
+
+
+// console.log(secondsInAHour);
+// console.log(secondsInADay);
+// console.log(secondsInAYear);
+// console.log(veronikaspapaAge * secondsInAYear);
+
+
+var hey = "hEy DuDe!";
+
+var newHey = hey[0].toUpperCase() + hey.slice(1).toLowerCase();
+
+out = document.getElementById("output")
+out.innerText = newHey;
+
+var tobi = "Tobi ist cool, und du nicht."
+
+alert(tobi);
