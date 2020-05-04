a2fd80f2b65d58a5139d9900af0bd03311b56cfd
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-05-04 21:48:33 +0200

    arrays

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
-// var drawCats = function (howManyTimes) {
-//   for (var i = 1; i <= howManyTimes; i++) {
-//     console.log(`Meow #${i} =^.^=`);
-//   }
-// };
-// drawCats(8);
+var newHey;
+var start = "Der Kater Tobi mag essen: " ;
+var finish = ["Fisch", "Butter", "Futter"];
+finish.push("Sahne")
+finish.unshift("Fleisch");
+newHey = start + finish.join(", ").bold() + ".";
 
-// var secondsInAMinute = 60;
-// var minutesInAHour = 60;
-// var secondsInAHour = secondsInAMinute * minutesInAHour;
-// var hoursInADay = 24;
-// var secondsInADay = secondsInAHour * hoursInADay;
-// var daysInAYear = 365;
-// var secondsInAYear = secondsInADay * daysInAYear;
-// var veronikasAge = 10;
-// var veronikaspapaAge = 48;
+var whatEat;
+var catMeal = 52;
+var time = prompt("What time is it?");
 
-
-// console.log(secondsInAHour);
-// console.log(secondsInADay);
-// console.log(secondsInAYear);
-// console.log(veronikaspapaAge * secondsInAYear);
-
-
-var hey = "hEy DuDe!";
-
-var newHey = hey[0].toUpperCase() + hey.slice(1).toLowerCase();
+if (catMeal > 0) {
+  whatEat = "We have the food";
+  if (time > 7) {
+    whatEat += " " + finish[0].bold();
+  } else {
+    whatEat += " " + finish[finish.length - 1].bold();
+  }
+} else {
+  whatEat = "We haven't the food";
+}
 
 out = document.getElementById("output")
-out.innerText = newHey;
+out.innerHTML = newHey;
+eat = document.getElementById("eat")
+eat.innerText = whatEat;
 
 var tobi = "Tobi ist cool, und du nicht."
+// alert(tobi)
 
-alert(tobi);
